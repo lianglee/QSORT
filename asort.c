b5aeb9e60071efde636d930eeae1576cdd163e7b
Arsalan Shah <manactric@gmail.com>
2016-09-07 14:43:35 +0500

    Update asort.c

--- asort.c
+++ asort.c
@@ -6,7 +6,6 @@
  * @copyright: 2016 SOFTLAB24 LIMITED
  * @license:   General Public Licence http://www.opensource-socialnetwork.org/licence
  * @link:      http://www.opensource-socialnetwork.org/
- * BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
  * YOUR USE OF THIS CODE SHALL BE GOVERNED BY SWISS LAW AND SHALL BE SUBJECT TO THE EXCLUSIVE JURISDICTION OF THE SWISS COURTS.
  */
  
